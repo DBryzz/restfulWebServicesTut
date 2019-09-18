49d5adb6833ff066ec7dd4e66a634c0de5f59b90
= <=>
2019-09-18 09:39:09 +0100

    First commit-helloworld restcontroller complete

---
+++ restful-web-services/src/main/HelloWorldBean.java
@@ -0,0 +1,19 @@
+package com.bryzz.rest.webservices.restfulwebservices;
+
+public class HelloWorldBean {
+	
+	private String message;
+	
+	public HelloWorldBean(String message) {
+		this.message = message;
+	}
+	
+	public void setMessage(String message) {
+		this.message = message;
+	}
+
+	@Override
+	public String toString() {
+		return String.format("HelloWorldBean [message=%s]", message);
+	}
+}
