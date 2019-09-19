d4a0142dda38b0125b1df49d221a817c76f388d9
= <=>
2019-09-19 16:03:30 +0100

    Internationalization

--- restful-web-services/src/main/java/com/bryzz/rest/webservices/restfulwebservices/RestfulWebServicesApplication.java
+++ restful-web-services/src/main/java/com/bryzz/rest/webservices/restfulwebservices/RestfulWebServicesApplication.java
@@ -1,7 +1,14 @@
 package com.bryzz.rest.webservices.restfulwebservices;
 
+import java.util.Locale;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.support.ResourceBundleMessageSource;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.i18n.AcceptHeaderLocaleResolver;
+import org.springframework.web.servlet.i18n.SessionLocaleResolver;
 
 @SpringBootApplication
 public class RestfulWebServicesApplication {
@@ -10,4 +17,25 @@ public class RestfulWebServicesApplication {
 		SpringApplication.run(RestfulWebServicesApplication.class, args);
 	}
 
+	
+	@Bean
+	public LocaleResolver localeResolver() {
+		AcceptHeaderLocaleResolver localeResolver = new AcceptHeaderLocaleResolver();  //AcceptHeaderLocaleResolver instead of SessionLocaleResolver
+		localeResolver.setDefaultLocale(Locale.US);
+		return localeResolver;
+	}
+	
+	/*
+	 * Alternative found in application.properties file
+	 * 
+	 * 
+	 * @Bean 
+	 * public ResourceBundleMessageSource messageSource() {
+	 * ResourceBundleMessageSource messageSource = new
+	 * ResourceBundleMessageSource(); messageSource.setBasename("messages"); return
+	 * messageSource;
+	 * 
+	 * }
+	 */
+	
 }
