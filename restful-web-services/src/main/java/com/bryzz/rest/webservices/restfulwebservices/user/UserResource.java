36a2244a703bc1f4eddd7d4056fc529fd03bd7fe
= <=>
2019-09-19 14:18:19 +0100

    first implementation of HATEOAS - Hypermedia As The Engine Of Application State

--- restful-web-services/src/main/java/com/bryzz/rest/webservices/restfulwebservices/user/UserResource.java
+++ restful-web-services/src/main/java/com/bryzz/rest/webservices/restfulwebservices/user/UserResource.java
@@ -1,11 +1,14 @@
 package com.bryzz.rest.webservices.restfulwebservices.user;
 
+import static org.springframework.hateoas.mvc.ControllerLinkBuilder.*;
 import java.net.URI;
 import java.util.List;
 
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.hateoas.Resource;
+import org.springframework.hateoas.mvc.ControllerLinkBuilder;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -28,13 +31,18 @@ public class UserResource {
 	}
 	
 	@GetMapping("/users/{id}")
-	public User retrieveUser(@PathVariable int id) {
+	public Resource<User> retrieveUser(@PathVariable int id) {
 		User user = service.findOne(id);
 		if(user == null) {
 			throw new UserNotFoundException("id- "+id);
 		}
 		
-		return user;
+		
+		Resource<User> resource = new Resource<User>(user);
+		ControllerLinkBuilder linkTo = linkTo(methodOn(this.getClass()).retrieveAllUsers());
+		resource.add(linkTo.withRel("all-users"));
+		
+		return resource;
 	}
 	
 	@PostMapping("/users")
